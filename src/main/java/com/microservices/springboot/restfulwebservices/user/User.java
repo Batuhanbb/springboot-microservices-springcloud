dcc45e7b4a37a0615be21b89203a6ad0a73be472
Bayir <batuhan.bayir@aegon.com.tr>
2022-05-27 17:18:06 +0300

    Various features have been added

--- src/main/java/com/microservices/springboot/restfulwebservices/user/User.java
+++ src/main/java/com/microservices/springboot/restfulwebservices/user/User.java
@@ -1,17 +1,33 @@
 package com.microservices.springboot.restfulwebservices.user;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
-
+@Entity
 public class User {
-
+@Id
     private int id;
 @Size(min = 2, message = "Name must be atleast 2 characters")
     private String name;
 @Past(message = "Date must be past")
     private Date date;
+@OneToMany(mappedBy = "user")
+private List<Post> post;
+protected User(){}
+
+    public List<Post> getPost() {
+        return post;
+    }
+
+    public void setPost(List<Post> post) {
+        this.post = post;
+    }
 
     @Override
     public boolean equals(Object o) {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+CREATE TABLE ROOM(
+                     ROOM_ID INTEGER PRIMARY KEY,
+                     ENTER VARCHAR('50') NOT NULL,
+                     NAME VARCHAR(23) NOT NULL
+);
+
+insert into ROOM values(1, '17/12/2015', 'BATU');
+insert into ROOM values(2, '17/12/2015', 'BATUKE');
+insert into ROOM values(3, '17/12/2015', 'BATUHANKE');
